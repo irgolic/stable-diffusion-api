32a3d3697d99df7175f7a05ffdaa0f8eb86e4d1a
Rafael Irgolic <hello@irgolic.com>
2022-10-16 15:31:52 -0400

    blob_repo: Retype as bytes instead of Union[str, bytes]

--- stable_diffusion_server/engine/repos/blob_repo.py
+++ stable_diffusion_server/engine/repos/blob_repo.py
@@ -9,10 +9,10 @@ BlobId: TypeAlias = str
 
 
 class BlobRepo:
-    def put_blob(self, data: Union[str, bytes]) -> BlobId:
+    def put_blob(self, data: bytes) -> BlobId:
         raise NotImplementedError
 
-    def get_blob(self, blob_id: BlobId) -> Optional[Union[str, bytes]]:
+    def get_blob(self, blob_id: BlobId) -> Optional[bytes]:
         raise NotImplementedError
 
     def get_blob_url(self, blob_id: BlobId) -> str:
@@ -23,12 +23,12 @@ class InMemoryBlobRepo(BlobRepo):
     def __init__(self):
         self._blobs = {}
 
-    def put_blob(self, data: Union[str, bytes]) -> BlobId:
+    def put_blob(self, data: bytes) -> BlobId:
         blob_id = BlobId(len(self._blobs))
         self._blobs[blob_id] = data
         return blob_id
 
-    def get_blob(self, blob_id: BlobId) -> Optional[Union[str, bytes]]:
+    def get_blob(self, blob_id: BlobId) -> Optional[bytes]:
         return self._blobs.get(blob_id, None)
 
     def get_blob_url(self, blob_id: BlobId) -> str:
@@ -40,12 +40,12 @@ class RedisBlobRepo(BlobRepo):
     def __init__(self):
         self.redis = get_redis()
 
-    def put_blob(self, data: Union[str, bytes]) -> BlobId:
+    def put_blob(self, data: bytes) -> BlobId:
         blob_id = BlobId(uuid.uuid4())
         self.redis.set(blob_id, data)
         return blob_id
 
-    def get_blob(self, blob_id: BlobId) -> Optional[Union[str, bytes]]:
+    def get_blob(self, blob_id: BlobId) -> Optional[bytes]:
         return self.redis.get(blob_id)
 
     def get_blob_url(self, blob_id: BlobId) -> str:
