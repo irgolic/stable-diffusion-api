5498149013c5335739a071729bca3439dd067175
Rafael Irgolic <hello@irgolic.com>
2022-10-16 15:32:21 -0400

    base_tests: Fixup
    - use small testing model on huggingface
    - also test img2img

--- stable_diffusion_server/api/tests/base.py
+++ stable_diffusion_server/api/tests/base.py
@@ -17,11 +17,13 @@ class BaseTestApp:
         cls.client = cls.get_client()
 
     @pytest.mark.asyncio
-    async def test_txt2img(self):
+    async def test_txt2img_2img(self):
         async with self.client.websocket_connect() as ws:
             params = {
+                "model_id": "hf-internal-testing/tiny-stable-diffusion-pipe",
                 "prompt": "corgi wearing a top hat",
                 "steps": 2,
+                "safety_filter": False,
             }
             response = self.client.post('/txt2img', json=params)
             assert response.status_code == 200
@@ -48,18 +50,18 @@ class BaseTestApp:
                 'image': {
                     'blob_id': mock.ANY,
                     'link': mock.ANY,
-                    'format': 'png',
                     'parameters_used': {
-                        'model_id': 'CompVis/stable-diffusion-v1-4',
+                        'task_type': 'txt2img',
+                        'model_id': 'hf-internal-testing/tiny-stable-diffusion-pipe',
                         'model_repository': 'huggingface',
                         'prompt': 'corgi wearing a top hat',
                         'negative_prompt': None,
                         'steps': 2,
                         'guidance': 7.5,
-                        'num_images': 1,
                         'scheduler': 'plms',
                         'width': 512,
                         'height': 512,
+                        "safety_filter": False,
                     },
                 }
             }
@@ -71,3 +73,55 @@ class BaseTestApp:
             response = self.client.get(f'/task/{task_id}')
             assert response.status_code == 200
             assert response.json() == expected_event
+
+            generated_image_blob_id = ws_event['image']['blob_id']
+
+            # run the generated image through img2img
+            response = self.client.post(
+                '/img2img',
+                json={
+                    "initial_image": generated_image_blob_id,
+                } | params
+            )
+            assert response.status_code == 200
+            task_id = response.json()
+
+            # pending event
+            ws_event = json.loads(json.loads(await ws.recv()))
+            assert ws_event == {
+                'event_type': 'pending',
+                'task_id': task_id,
+            }
+
+            # started event
+            ws_event = json.loads(json.loads(await ws.recv()))
+            assert ws_event == {
+                'event_type': 'started',
+                'task_id': task_id,
+            }
+
+            # finished event
+            expected_event = {
+                'event_type': 'finished',
+                'task_id': task_id,
+                'image': {
+                    'blob_id': mock.ANY,
+                    'link': mock.ANY,
+                    'parameters_used': {
+                        'task_type': 'img2img',
+                        'model_id': 'hf-internal-testing/tiny-stable-diffusion-pipe',
+                        'model_repository': 'huggingface',
+                        'prompt': 'corgi wearing a top hat',
+                        'negative_prompt': None,
+                        'steps': 2,
+                        'guidance': 7.5,
+                        'scheduler': 'plms',
+                        'initial_image': generated_image_blob_id,
+                        "safety_filter": False,
+                        "strength": 0.8,
+                    },
+                }
+            }
+
+            ws_event = json.loads(json.loads(await ws.recv()))
+            assert ws_event == expected_event
