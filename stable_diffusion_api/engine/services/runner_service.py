6f955c1874b0202dc9787776efc1d5aa8970784c
Rafael Irgolic <hello@irgolic.com>
2022-10-30 01:04:19 -0400

    runner_service: Use in-repo lpw_stable_diffusion pipeline

--- stable_diffusion_api/engine/services/runner_service.py
+++ stable_diffusion_api/engine/services/runner_service.py
@@ -80,6 +80,10 @@ class RunnerService:
             'pretrained_model_name_or_path': params.model
         }
 
+        # get path to directory containing pipeline
+        pipeline_dir = os.path.join(os.path.dirname(__file__), '..', 'pipelines', 'lpw_stable_diffusion')
+        pipeline_kwargs['custom_pipeline'] = pipeline_dir
+
         # set token
         if "HUGGINGFACE_TOKEN" in os.environ:
             pipeline_kwargs['use_auth_token'] = os.environ["HUGGINGFACE_TOKEN"]
@@ -124,9 +128,6 @@ class RunnerService:
         )
 
         # prepare pipeline
-        pipeline_kwargs.update(
-            custom_pipeline=params._pipeline,
-        )
         if isinstance(params, Txt2ImgParams):
             pipe_kwargs.update(
                 height=params.height,
